ee817f9767b8a7e4c5c27fe6d913ee8b70bd0b72
sharvil <sharvil.tmbs25@gmail.com>
2026-04-23 11:42:24 +0530

    Delete application/config directory

--- application/config/razorpay.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['razorpay_key_id'] = 'rzp_test_SQegebn7NHi2HZ'; // replace with your key id
-$config['razorpay_key_secret'] = '6DHNi6FGHUTYpnrq9Zfzm78p'; // replace with your key secret
-$config['razorpay_mode'] = 'test'; // 'test' or 'live'
